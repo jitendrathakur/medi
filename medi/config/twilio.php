e4a716dcc815ebd01ef20e39003cb3e9d481c809
Yogesh <cis@CISM407.(none)>
2013-11-26 21:41:28 +0530

    added twilio

---
+++ medi/config/twilio.php
@@ -0,0 +1,44 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	/**
+	* Name:  Twilio
+	*
+	* Author: Ben Edmunds
+	*		  ben.edmunds@gmail.com
+	*         @benedmunds
+	*
+	* Location:
+	*
+	* Created:  03.29.2011
+	*
+	* Description:  Twilio configuration settings.
+	*
+	*
+	*/
+
+	/**
+	 * Mode ("sandbox" or "prod")
+	 **/
+	$config['mode']   = 'sandbox';
+
+	/**
+	 * Account SID
+	 **/
+	$config['account_sid']   = 'AC11f57b3d009bab2f44110703654835a4';
+
+	/**
+	 * Auth Token
+	 **/
+	$config['auth_token']    = '36849398dc23d0adfe0433a6dd1a566b';
+
+	/**
+	 * API Version
+	 **/
+	$config['api_version']   = '2010-04-01';
+
+	/**
+	 * Twilio Phone Number
+	 **/
+	$config['number']        = '9713485910';
+
+
+/* End of file twilio.php */
